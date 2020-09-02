c5d5bcc8006e383acdf5c2047fb87aa56aaa506b
Pravallika Nakarikanti <pravallika-1305@github.com>
2020-09-02 18:16:09 +0530

    added imageupload using multer

---
+++ imageUpload/uploadImage/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DragDropFileUploadDirective } from './drag-drop-file-upload.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DragDropComponent,
+    DragDropFileUploadDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
